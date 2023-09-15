305479767d2058c7d9b1a7dcc224c929519b716c
Simon Stock <stock@fzi.de>
2023-09-16 01:38:20 +0200

    added the option to change from light to dark mode and back with the settings screen in the top right corner. Furthermore, added permissions for macOS to load images from online.

--- lib/src/timeline_view/timeline_view.dart
+++ lib/src/timeline_view/timeline_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:timeline_list/timeline.dart';
 import 'package:timeline_list/timeline_model.dart';
+import '../settings/settings_view.dart';
 import 'data.dart';
 
 class TimelinePage extends StatefulWidget {
@@ -46,7 +47,18 @@ class _TimelinePageState extends State<TimelinePage> {
               ),
             ]),
         appBar: AppBar(
-          title: Text(widget.title),
+          title: const Text('Sample Items'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {
+                // Navigate to the settings page. If the user leaves and returns
+                // to the app after it has been killed while running in the
+                // background, the navigation stack is restored.
+                Navigator.restorablePushNamed(context, SettingsView.routeName);
+              },
+            ),
+          ],
         ),
         body: PageView(
           onPageChanged: (i) => setState(() => pageIx = i),
