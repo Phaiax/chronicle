44efb88ab66df8ecba87fb6ff46c2275c157774c
Marius Wanner <mariuswanner@gmx.de>
2023-09-17 01:27:29 +0200

    add openAI API to email author

--- equick.py
+++ equick.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
+
 import pyautogui
 import pyperclip
 import time
+import os
 from pynput import mouse
 import platform
+import openai
 
 def on_click(x, y, button, pressed):
     if button == mouse.Button.middle and pressed:
@@ -16,7 +20,7 @@ def handle_text():
         ctrl = 'command'
     else:
         print("UNSUPPORTED PLATFORM")
-        exit(-1)
+        ctrl = 'ctrl'
 
     time.sleep(0.4)
     # Step 1: Get the currently selected text
@@ -40,9 +44,39 @@ def handle_text():
     pyautogui.hotkey(ctrl, 'up', interval=0.1)
     time.sleep(0.1)
 
-    # Step 6: Paste the reversed text
-    reversed_text = selected_text[::-1]
-    pyperclip.copy(reversed_text)
+
+    promt = f"""
+    Always be polite, professional, not too formal and always try to match the tone of the mails from the sender.
+    You will write as Simon.
+    The Content you should write about is: {selected_text}
+    The History of the conversation is as follows: {all_text}
+    Create a reply. Do not write a "Subject" line, directly write the mail.
+    """
+
+    # Step 6: Get the generated text from openAI
+    messages = [
+        {"role": "system", "content": "You are my assistant who will write Mails in my name, which is Simon"},
+        {"role": "user", "content": promt},
+    ]
+    stream = True
+    resp = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=messages,
+    stream=stream,
+    )
+    if stream:
+        content = []
+        for delta_resp in resp:
+            finished = delta_resp['choices'][0]['finish_reason'] is not None
+            if "content" in  delta_resp['choices'][0]['delta']:
+                word = delta_resp['choices'][0]['delta']['content']
+                content.append(word)
+                print(word, end="", flush=True)
+            pass
+        print()
+        content = ''.join(content)
+
+    pyperclip.copy(content)
     pyautogui.hotkey(ctrl, 'v', interval=0.1)
     time.sleep(0.1)
     pyautogui.press('enter', interval=0.1)
@@ -50,6 +84,10 @@ def handle_text():
     time.sleep(0.1)
 
 def main():
+    #prep openai
+    openai.organization = os.getenv("OPENAI_ORG")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
     # Start listening for mouse events
     with mouse.Listener(on_click=on_click) as listener:
         listener.join()
